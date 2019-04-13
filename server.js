2e303a8a649e2d9dcb164e06ec3cf38e8d251e4a
Junior Silva <jros2.silva@gmail.com>
2019-04-13 01:14:35 -0300

    Adicionando a biblioteca de log: Morgan

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
-
+// Middleware
+app.use(morgan("dev"));
 app.listen("3000", error => {
   if (error) throw error;
   console.log("Server online");
