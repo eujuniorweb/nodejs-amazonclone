60eb1331e1da607babdb279932c5f2e781bc8284
Junior Silva <jros2.silva@gmail.com>
2019-04-14 14:21:24 -0300

    configurando o template engine ejs

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ const routes = express.Router();
 
 const UserController = require("./controllers/UserController");
 
+routes.get("/", (req, res) => {
+  res.render("home");
+});
 routes.post("/users", UserController.store);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const ejs = require("ejs");
+const ejsMate = require("ejs-mate");
+const path = require("path");
 
 const app = express();
 // Conectando ao MongoDb
@@ -17,6 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes"));
 app.use(morgan("dev"));
+app.engine("ejs", ejsMate);
+// view engine setup
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
 
 app.listen("3000", error => {
   if (error) throw error;
