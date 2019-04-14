b8236fcdd866389e7a1d04abc53e907f23e7f6bb
Junior Silva <jros2.silva@gmail.com>
2019-04-13 21:41:54 -0300

    Schema de usuário, instalação do mongoose e bcrypt-nodejs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
+
 // Middleware
 app.use(morgan("dev"));
 app.listen("3000", error => {
---
+++ src/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const Schema = mongoose.Schema;
+
+// Atributos do Esquema de usuário, campos, caracteristicas
+const UserSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: String,
+
+  profile: {
+    name: { type: String, default: "" },
+    picture: { type: String, default: "" }
+  },
+  address: String,
+  history: [
+    {
+      date: Date,
+      paid: { type: Number, default: 0 }
+      //   item:{type:Schema.Types.ObjectId, ref:''}
+    }
+  ]
+});
+// Hash a senha antes de salvá-la no banco de dados
+UserSchema.pre("save", next => {
+  let user = this;
+  if (!user.isModified("password")) return next();
+  bcrypt.genSalt(10, (error, salt) => {
+    if (error) return next(error);
+    bcrypt.hash(user.password, salt, null, (error, hash) => {
+      if (error) return next(error);
+      user.password = hash;
+      next();
+    });
+  });
+});
+// comparar a senha no banco de dados com a senha que o usuário digitou
+UserSchema.methods.comparePassword = password => {
+  return bcrypt.compareSync(password, this.password);
+};
+module.exports = mongoose.model("User", UserSchema);
