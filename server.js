e79c7bfa5894154bab17da44bb18d5b46b034577
Junior Silva <jros2.silva@gmail.com>
2019-04-13 21:42:52 -0300

    Conectando ao mongo DB Atlas

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const app = express();
-
+// Conectando ao MongoDb
+mongoose.connect(
+  "mongodb+srv://developer:developer@cluster0-mclee.mongodb.net/amazonclone?retryWrites=true",
+  { useNewUrlParser: true },
+  error => {
+    if (error) console.log(error);
+    console.log("Conectado ao Mongo");
+  }
+);
 // Middleware
 app.use(morgan("dev"));
 app.listen("3000", error => {
